d6defed0a952278556d34bd460672e4a81c2c521
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2020-06-03 23:58:35 -0300

    Backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,44 +1,13 @@
-import express, { json } from 'express'
+import express from 'express'
+import cors from 'cors'
+import path from 'path'
+import routes from './routes'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
-
-
-
-const users = [
-    'Diego',   // 0
-    'Cleiton', // 1 
-    'Robson',  // 2
-    'Daniel'   // 3
-]
-
-app.get('/users', (request, response) => {
-    const search = String(request.query.search)
-    const filteredUsers = search ? users.filter(user => user.includes(search)) : users
-
-    // JSON
-    response.json(filteredUsers)
-})
-
-app.get('/users/:id', (request, response) => {
-    const id = Number(request.params.id)
-
-    return response.json(users[id])
-})
-
-app.post('/users', (request, response) => {
-    const data = request.body
-    
-    const user = {
-        name: data.name,
-        email: data.email
-    }
-
-    // Para retornar uma resposta para o front-end pode executado o response.methodName('someThingHere').
-    // Mas é sempre bom colocar um return nessa linha. Assim o processamento da nossa requisição para forçadamente nessa linha.
-    // Ou seja, caso não for colocado o return, os códigos após o response.methodName(...) continuarão sendo executados.
-    return response.json(user)
-})
+app.use(routes)
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
 app.listen(3333)
